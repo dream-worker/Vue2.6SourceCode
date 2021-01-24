852ea1559f45880e736680b45649f5da4e67c60b
yuelan.yang <yuelan.yang@causacloud.com>
2021-01-24 19:34:58 +0800

    feat: init

---
+++ src/core/instance/index.js
@@ -0,0 +1,28 @@
+import { initMixin } from './init'
+import { stateMixin } from './state'
+import { renderMixin } from './render'
+import { eventsMixin } from './events'
+import { lifecycleMixin } from './lifecycle'
+import { warn } from '../util/index'
+
+function Vue (options) {
+  console.log(1)
+  console.log("process.env.NODE_ENV", process.env.NODE_ENV);
+  if (process.env.NODE_ENV !== 'production' &&
+    !(this instanceof Vue)
+  ) {
+    warn('Vue is a constructor and should be called with the `new` keyword')
+  } else {
+    console.log(3);
+  }
+  console.log(2);
+  this._init(options)
+}
+
+initMixin(Vue)
+stateMixin(Vue)
+eventsMixin(Vue)
+lifecycleMixin(Vue)
+renderMixin(Vue)
+
+export default Vue
