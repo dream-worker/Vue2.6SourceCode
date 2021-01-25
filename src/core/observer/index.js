9b62cf44c248bcd26cb17b1d7a96e06a7a6246f7
yuelan.yang <yuelan.yang@causacloud.com>
2021-01-25 10:24:10 +0800

    topic: observe

--- src/core/observer/index.js
+++ src/core/observer/index.js
@@ -38,21 +38,25 @@ export class Observer {
   value: any;
   dep: Dep;
   vmCount: number; // number of vms that have this object as root $data
-
   constructor (value: any) {
-    this.value = value
-    this.dep = new Dep()
-    this.vmCount = 0
-    def(value, '__ob__', this)
+    this.value = value;
+    this.dep = new Dep();
+    this.vmCount = 0;
+    // @todo: 先调用 def
+    def(value, "__ob__", this);
+    // 根据传入  value  的不同类型，做相应处理
+    // 数组：原型覆盖
     if (Array.isArray(value)) {
       if (hasProto) {
-        protoAugment(value, arrayMethods)
+        protoAugment(value, arrayMethods);
       } else {
-        copyAugment(value, arrayMethods, arrayKeys)
+        copyAugment(value, arrayMethods, arrayKeys);
       }
-      this.observeArray(value)
+      this.observeArray(value);
     } else {
-      this.walk(value)
+      // 如果是对象。
+      // 非数组：walk 遍历 value 的每一个 key
+      this.walk(value);
     }
   }
 
@@ -62,9 +66,12 @@ export class Observer {
    * value type is Object.
    */
   walk (obj: Object) {
-    const keys = Object.keys(obj)
+    const keys = Object.keys(obj);
+    // 非数组：walk 遍历 value 的每一个 key
     for (let i = 0; i < keys.length; i++) {
-      defineReactive(obj, keys[i])
+      // 每一个 key 都执行  defineReactive
+      // 拦截这些属性
+      defineReactive(obj, keys[i]);
     }
   }
 
@@ -111,9 +118,16 @@ export function observe (value: any, asRootData: ?boolean): Observer | void {
   if (!isObject(value) || value instanceof VNode) {
     return
   }
+  // 作用： 获取  Observer 实例
+  // 执行数据响应式过程中，每出现一个对象，就创建一个 Observer 实例
+  // console 一个响应式的 数据，就会看到携带的 __ob__ 属性
+  // 由此来标记：该数据是响应式数据
   let ob: Observer | void
+  // 如果已经是响应式数据，直接返回其 ob
   if (hasOwn(value, '__ob__') && value.__ob__ instanceof Observer) {
-    ob = value.__ob__
+    // value.__ob__ 指向 ob
+    // ob 就是  Observer 实例 ob = new Observer(value)
+    ob = value.__ob__;
   } else if (
     shouldObserve &&
     !isServerRendering() &&
@@ -121,6 +135,7 @@ export function observe (value: any, asRootData: ?boolean): Observer | void {
     Object.isExtensible(value) &&
     !value._isVue
   ) {
+    // 如果没有携带，创建一个
     ob = new Observer(value)
   }
   if (asRootData && ob) {
@@ -132,65 +147,90 @@ export function observe (value: any, asRootData: ?boolean): Observer | void {
 /**
  * Define a reactive property on an Object.
  */
-export function defineReactive (
+// 调用该函数时的参数：看类的定义
+export function defineReactive(
   obj: Object,
   key: string,
   val: any,
   customSetter?: ?Function,
   shallow?: boolean
 ) {
-  const dep = new Dep()
+  // key 对应的管家： dep
+  const dep = new Dep();
 
-  const property = Object.getOwnPropertyDescriptor(obj, key)
+  // Object.getOwnPropertyDescriptor(obj, obj.property): 返回 true | false
+  // 方法返回指定对象上一个自有属性对应的属性描述符。
+  // （自有属性指的是直接赋予该对象的属性，不需要从原型链上进行查找的属性）
+  // 示例：
+  // const object1 = {
+  //   property1: 42,
+  // };
+
+  // const descriptor1 = Object.getOwnPropertyDescriptor(object1, "property1");
+
+  // console.log(descriptor1);
+  // // expected output: Object { value: 42, writable: true, enumerable: true, configurable: true }
+
+  // console.log(descriptor1.configurable);
+  // // expected output: true
+
+  // console.log(descriptor1.value);
+  // // expected output: 42
+
+  const property = Object.getOwnPropertyDescriptor(obj, key);
+  // property： 是  obj 的 描述符对象
   if (property && property.configurable === false) {
-    return
+    return;
   }
 
   // cater for pre-defined getter/setters
-  const getter = property && property.get
-  const setter = property && property.set
+  // 如果是自有属性，非原型查找得到.用户设置了  set 方法
+  const getter = property && property.get;
+  const setter = property && property.set;
   if ((!getter || setter) && arguments.length === 2) {
-    val = obj[key]
+    val = obj[key];
   }
-
-  let childOb = !shallow && observe(val)
+  // 有可能需要递归：因为传入的 对象 是 obj: { info: user: { name: 'julie'}}
+  // observe(val) 返回的是实例： ob = new Observer(value);
+  // @todo：shallow
+  let childOb = !shallow && observe(val);
   Object.defineProperty(obj, key, {
     enumerable: true,
     configurable: true,
-    get: function reactiveGetter () {
-      const value = getter ? getter.call(obj) : val
+    get: function reactiveGetter() {
+      const value = getter ? getter.call(obj) : val;
       if (Dep.target) {
-        dep.depend()
+        dep.depend();
         if (childOb) {
-          childOb.dep.depend()
+          childOb.dep.depend();
           if (Array.isArray(value)) {
-            dependArray(value)
+            dependArray(value);
           }
         }
       }
-      return value
+      return value;
     },
-    set: function reactiveSetter (newVal) {
-      const value = getter ? getter.call(obj) : val
+    set: function reactiveSetter(newVal) {
+      const value = getter ? getter.call(obj) : val;
       /* eslint-disable no-self-compare */
       if (newVal === value || (newVal !== newVal && value !== value)) {
-        return
+        return;
       }
       /* eslint-enable no-self-compare */
-      if (process.env.NODE_ENV !== 'production' && customSetter) {
-        customSetter()
+      if (process.env.NODE_ENV !== "production" && customSetter) {
+        customSetter();
       }
       // #7981: for accessor properties without setter
-      if (getter && !setter) return
+      if (getter && !setter) return;
       if (setter) {
-        setter.call(obj, newVal)
+        setter.call(obj, newVal);
       } else {
-        val = newVal
+        val = newVal;
       }
-      childOb = !shallow && observe(newVal)
-      dep.notify()
-    }
-  })
+      childOb = !shallow && observe(newVal);
+      dep.notify();
+    },
+  });
 }
 
 /**
